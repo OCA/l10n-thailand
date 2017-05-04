fa908477d5734080f9d1e4b7afba03cbccfcdc6c
Kitti U <kittiu@gmail.com>
2017-05-04 21:45:52 +0700

    [FIX] remove base. from id of data file, add post_init_hook

--- l10n_th_address/__init__.py
+++ l10n_th_address/__init__.py
@@ -2,3 +2,14 @@
 # © 2017 Ecosoft (ecosoft.co.th).
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import models
+from odoo import api, SUPERUSER_ID
+
+
+def _update_data_noupdate(cr, registry):
+    cr.execute("""
+        update ir_model_data
+        set noupdate = true
+        where model in ('res.country.province',
+                        'res.country.district',
+                        'res.country.township')
+    """)
--- l10n_th_address/__manifest__.py
+++ l10n_th_address/__manifest__.py
@@ -32,6 +32,7 @@ Choosing Township will realize Province, District, and Zip Code
     ],
     'installable': True,
     'auto_install': False,
+    'post_init_hook': '_update_data_noupdate',
 }
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
