ed9516c31f6077164dd5bd595351b9e40f384da5
Kitti U <kittiu@gmail.com>
2017-05-12 19:35:40 +0700

    [FIX] remove base. from id of data file, add post_init_hook (#3)

--- l10n_th_address/__manifest__.py
+++ l10n_th_address/__manifest__.py
@@ -5,33 +5,19 @@
     'name': 'Partner Address in Thai',
     'version': '10.0.0.1.0',
     'category': 'Hidden',
-    'description': """
-
-Address for Thailand
-====================
-
-If country is selected as Thailand, new fields will show up.
-
-* Province (Chanwat)
-* District (Amphoe)
-* Township (Tambon)
-
-Choosing Township will realize Province, District, and Zip Code
-
-    """,
-    'author': 'Ecosoft',
-    'website': 'www.ecosoft.co.th',
+    'author': 'Ecosoft,Odoo Community Association (OCA)',
+    'website': 'http://ecosoft.co.th',
     'depends': ['sales_team'],
+    'license': 'AGPL-3',
     'data': [
-        'views/res_country_view.xml',
-        'views/res_partner_view.xml',
         'security/ir.model.access.csv',
         'data/res.country.province.csv',
         'data/res.country.district.csv',
         'data/res.country.township.csv',
+        'views/res_country_view.xml',
+        'views/res_partner_view.xml',
     ],
     'installable': True,
     'auto_install': False,
+    'post_init_hook': 'post_init_hook',
 }
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
