5e81e4e3ea88224060fb38cb6f2a01c32c4cb92c
Poonlap Veerathanabutr <poonlap@tanabutr.co.th>
2018-10-31 23:33:55 +0700

    Initial code/data to add province names for Thailand.

---
+++ l10n_nl_country_states/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+# Copyright (C) 2018 Tanabutr Co., Ltd. (<https://www.tanabutr.co.th>).
---
+++ l10n_nl_country_states/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2018 by Poonlap <poonlap@tanabutr.co.th>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': "Province names in Thailand",
+    'version': '1.0',
+    'depends': ['base'],
+    'author': "Poonlap Veerathanabutr",
+    'license': "AGPL-3"
+    'category': 'Localization',
+    'description': """
+    This module simply creates Thai province names (both English and Thai) into 'Fed States' database.
+    """,
+    'data': [
+       'data/res.country.state.csv',
+    ],
+}
