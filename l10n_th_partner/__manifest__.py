58b571e2ecc38af2f0ca5d23130d55a7713ea4cd
Kitti U <kittiu@gmail.com>
2019-03-08 15:38:22 +0700

    [ENH] readme for l10n_th_partner

--- l10n_th_partner/__manifest__.py
+++ l10n_th_partner/__manifest__.py
@@ -1,17 +1,16 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+# Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-
-{'name': 'Thai Localization - Partner',
- 'version': '12.0.1.0.0',
- 'author': 'Ecosoft,Odoo Community Association (OCA)',
- 'website': '???',
- 'license': 'AGPL-3',
- 'category': 'Localisation/Asia',
- 'depends': ['base',
-             ],
- 'data': ['views/res_partner_view.xml',
-          ],
- 'installable': True,
- 'development_status': '???',
- 'maintainers': ['kittiu'],
+{
+    'name': 'Thai Localization - Partner',
+    'version': '12.0.1.0.0',
+    'author': 'Ecosoft,Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/l10n-thailand',
+    'license': 'AGPL-3',
+    'category': 'Localisation/Asia',
+    'depends': ['base'],
+    'data': ['views/res_partner_view.xml'],
+    'installable': True,
+    'development_status': 'alpha',
+    'maintainers': ['kittiu'],
  }
--- l10n_th_partner/models/res_partner.py
+++ l10n_th_partner/models/res_partner.py
@@ -1,4 +1,5 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+# Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 from odoo import models, fields
 
 
