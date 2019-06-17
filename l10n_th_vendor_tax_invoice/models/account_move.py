df029136dcee5a99105d696d7f017841f6aa11ac
ps-tubtim <pimolnat.s@outlook.com>
2019-06-17 15:42:28 +0700

    [12.0][FIX]l10n_th_withholding_tax_cert_form

--- l10n_th_vendor_tax_invoice/models/account_move.py
+++ l10n_th_vendor_tax_invoice/models/account_move.py
@@ -130,3 +130,12 @@ class AccountPartialReconcile(models.Model):
         ctx = {'cash_basis_entry_move_line': move_line,
                'payment': payment}
         return self.with_context(ctx)
+
+    def create_tax_cash_basis_entry(self, percentage_before_rec):
+        res = super(AccountPartialReconcile, self).create_tax_cash_basis_entry(
+            percentage_before_rec)
+        move_line = self.env['account.move.line'].search([
+            ('move_id.tax_cash_basis_rec_id', '=', self.id)]).filtered(
+            lambda l: not l.invoice_tax_line_id)
+        move_line.unlink()
+        return res
