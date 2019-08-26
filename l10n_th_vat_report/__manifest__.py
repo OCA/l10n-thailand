33d671f24130adba694209b7b36e6ecba0cba64c
ps-tubtim <pimolnat.s@outlook.com>
2019-08-26 14:48:33 +0700

    [12.0][FIX]l10n_th_vat_report

--- l10n_th_vat_report/__manifest__.py
+++ l10n_th_vat_report/__manifest__.py
@@ -1,16 +1,16 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
 {
     'name': 'Thailand Localization - VAT Reports',
-    'version': '12.0.1.0.0',
+    'version': '12.0.1.0.1',
     'author': 'Ecosoft, Odoo Community Association (OCA)',
     'website': 'https://github.com/OCA/l10n-thailand',
     'license': 'AGPL-3',
     'category': 'Accounting',
     'depends': [
-        'report_xlsx',
-        'report_xlsx_helper',
         'date_range',
+        'report_xlsx_helper',
         'l10n_th_partner',
         'l10n_th_vendor_tax_invoice',
     ],
--- l10n_th_vat_report/readme/HISTORY.rst
+++ l10n_th_vat_report/readme/HISTORY.rst
@@ -3,3 +3,10 @@
 
 * Start of the history
 * Add Thai VAT Report
+
+12.0.1.0.1 (2019-09-02)
+~~~~~~~~~~~~~~~~~~~~~~~
+
+* Fix column swap in xlsx report
+* Fix wrong query database to report
+* Add test script
