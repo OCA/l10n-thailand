6c6bb823231dd417c79d8e1bbed7b1dc95c72571
kongrattapong <rattapong.chok@gmail.com>
2019-11-04 12:56:46 +0700

    [IMP] l10n_th_withholding_tax_cert, isort

--- l10n_th_withholding_tax_cert/wizard/create_withholding_tax_cert.py
+++ l10n_th_withholding_tax_cert/wizard/create_withholding_tax_cert.py
@@ -1,34 +1,39 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
-from odoo import models, fields, api, _
+from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
 
 class CreateWithholdingTaxCert(models.TransientModel):
-    _name = 'create.withholding.tax.cert'
-    _description = 'Create Withholding Tax Cert Wizard'
+    _name = "create.withholding.tax.cert"
+    _description = "Create Withholding Tax Cert Wizard"
 
     wt_account_ids = fields.Many2many(
-        comodel_name='account.account',
-        string='Withholing Tax Accounts',
+        comodel_name="account.account",
+        string="Withholing Tax Accounts",
         help="If accounts are specified, system will auto fill tax amount",
-        default=lambda self:
-        self.env['account.account'].search([('wt_account', '=', True)]),
+        default=lambda self: self.env["account.account"].search(
+            [("wt_account", "=", True)]
+        ),
     )
 
     @api.multi
     def create_wt_cert(self):
         self.ensure_one()
         ctx = self._context.copy()
-        if len(ctx.get('active_ids', [])) != 1:
-            raise ValidationError(_('Please select only 1 payment'))
-        ctx.update({'default_payment_id': ctx.get('active_id'),
-                    'wt_account_ids': self.wt_account_ids.ids})
+        if len(ctx.get("active_ids", [])) != 1:
+            raise ValidationError(_("Please select only 1 payment"))
+        ctx.update(
+            {
+                "default_payment_id": ctx.get("active_id"),
+                "wt_account_ids": self.wt_account_ids.ids,
+            }
+        )
         return {
-            'name': _("Create Withholding Tax Cert."),
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'withholding.tax.cert',
-            'type': 'ir.actions.act_window',
-            'context': ctx,
+            "name": _("Create Withholding Tax Cert."),
+            "view_type": "form",
+            "view_mode": "form",
+            "res_model": "withholding.tax.cert",
+            "type": "ir.actions.act_window",
+            "context": ctx,
         }
