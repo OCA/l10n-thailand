6f8331ff4cf1d9125187c43b956924d35d8e4e06
Poonlap Veerathanabutr <poonlap@tanabutr.co.th>
2019-11-05 08:54:17 +0700

    [IMP] l10n_th_partner: black, isort

--- l10n_th_partner/models/res_partner.py
+++ l10n_th_partner/models/res_partner.py
@@ -1,13 +1,11 @@
 # Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     branch = fields.Char(
-        string='Branch',
-        size=5,
-        help="Branch ID, e.g., 0000, 0001, ..."
+        string="Branch", size=5, help="Branch ID, e.g., 0000, 0001, ..."
     )
