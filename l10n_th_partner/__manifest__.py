8e8c97a0d02c76925cb68a784c166f4cfbcbc3ed
Poonlap Veerathanabutr <poonlap@tanabutr.co.th>
2019-11-05 08:54:17 +0700

    [IMP] l10n_th_partner: black, isort

--- l10n_th_partner/__manifest__.py
+++ l10n_th_partner/__manifest__.py
@@ -2,15 +2,15 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 {
-    'name': 'Thai Localization - Partner',
-    'version': '12.0.1.0.0',
-    'author': 'Ecosoft,Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/l10n-thailand',
-    'license': 'AGPL-3',
-    'category': 'Localisation/Asia',
-    'depends': ['base'],
-    'data': ['views/res_partner_view.xml'],
-    'installable': True,
-    'development_status': 'alpha',
-    'maintainers': ['kittiu'],
+    "name": "Thai Localization - Partner",
+    "version": "13.0.1.0.0",
+    "author": "Ecosoft,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-thailand",
+    "license": "AGPL-3",
+    "category": "Localisation/Asia",
+    "depends": ["base"],
+    "data": ["views/res_partner_view.xml"],
+    "installable": True,
+    "development_status": "alpha",
+    "maintainers": ["kittiu"],
 }
