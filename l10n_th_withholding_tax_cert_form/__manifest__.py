0c9719250595f003e4bab687bd8e8974b48e3fb8
kongrattapong <rattapong.chok@gmail.com>
2019-11-05 17:09:06 +0700

    [MIG] l10n_th_withholding_tax_cert_form: Migration to 13.0

--- l10n_th_withholding_tax_cert_form/__manifest__.py
+++ l10n_th_withholding_tax_cert_form/__manifest__.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# Copyright 2019 Ecosoft Co., Ltd (https://ecosoft.co.th)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 {
@@ -6,7 +6,7 @@
     "version": "13.0.1.0.0",
     "author": "Ecosoft,Odoo Community Association (OCA)",
     "license": "AGPL-3",
-    "website": "https://github.com/OCA/l10n-thailand/",
+    "website": "https://github.com/OCA/l10n-thailand",
     "category": "Report",
     "depends": ["account", "web", "l10n_th_withholding_tax_cert"],
     "external_dependencies": {"python": ["num2words"]},
