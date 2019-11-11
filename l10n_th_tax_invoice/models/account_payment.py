c33c0b537bd7aee3aee66b32821de9297672aa52
Kitti U <kittiu@gmail.com>
2019-11-11 15:10:46 +0700

    [13.0][ADD] l10n_th_tax_invoice

---
+++ l10n_th_tax_invoice/models/account_payment.py
@@ -0,0 +1,32 @@
+# Copyright 2019 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+from odoo import models, fields, api, _
+from odoo.exceptions import UserError
+
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+
+    to_clear_tax = fields.Boolean(
+        default=False,
+        copy=False,
+        help="When defer journal entry posting, this will show button",
+    )
+    tax_invoice_ids = fields.One2many(
+        comodel_name='account.move.tax.invoice',
+        inverse_name='payment_id',
+        copy=False,
+    )
+
+    def clear_tax_cash_basis(self):
+        for payment in self:
+            for tax_invoice in payment.tax_invoice_ids:
+                if not tax_invoice.tax_invoice_number or \
+                        not tax_invoice.tax_invoice_date:
+                    raise UserError(
+                        _('Please fill in tax invoice and tax date'))
+            payment.write({'to_clear_tax': False})
+            move = payment.tax_invoice_ids.mapped('move_id')
+            move.ensure_one()
+            move.post()
+        return True
