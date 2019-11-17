55e9e7f662b54564d4df068d8c099d63dbf1b049
kittiu <kittiu@gmail.com>
2019-11-17 16:39:15 +0700

    [13.0][IMP] l10n_th_tax_invoice: black, isort

--- l10n_th_tax_invoice/__manifest__.py
+++ l10n_th_tax_invoice/__manifest__.py
@@ -2,21 +2,21 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 {
-    'name': 'Thai Localization - Account Tax Invoice',
-    'version': '13.0.1.0.0',
-    'author': 'Ecosoft,Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'website': 'https://github.com/OCA/l10n-thailand/',
-    'category': 'Localization / Accounting',
-    'depends': ['account'],
-    'data': [
-        'security/ir.model.access.csv',
-        'wizard/wizard_tax_adjustments_view.xml',
-        'views/account_move_view.xml',
-        'views/account_payment_view.xml',
+    "name": "Thai Localization - Account Tax Invoice",
+    "version": "13.0.1.0.0",
+    "author": "Ecosoft,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/l10n-thailand/",
+    "category": "Localization / Accounting",
+    "depends": ["account"],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/wizard_tax_adjustments_view.xml",
+        "views/account_move_view.xml",
+        "views/account_payment_view.xml",
         # 'views/account_payment_view.xml',
     ],
-    'installable': True,
-    'development_status': 'alpha',
-    'maintainers': ['kittiu'],
+    "installable": True,
+    "development_status": "alpha",
+    "maintainers": ["kittiu"],
 }
