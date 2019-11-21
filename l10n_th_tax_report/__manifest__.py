05480f4ca3252bd31f5d0e20e4ca4c151363758b
kongrattapong <rattapong.chok@gmail.com>
2019-11-21 12:03:26 +0700

    [ADD][13.0] l10n_tax_report - PDF&HTML report

---
+++ l10n_th_tax_report/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2019 Ecosoft Co., Ltd (https://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Thailand Localization - TAX Reports",
+    "version": "13.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-thailand",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "depends": [
+        "date_range",
+        "report_xlsx_helper",
+        "l10n_th_partner",
+        "l10n_th_tax_invoice",
+    ],
+    "data": [
+        "data/paper_format.xml",
+        "data/report_data.xml",
+        "reports/tax_report.xml",
+        "wizard/tax_report_wizard_view.xml",
+    ],
+    "installable": True,
+}
---
+++ l10n_th_tax_report/readme/HISTORY.rst
@@ -0,0 +1,17 @@
+12.0.1.0.0 (2019-08-05)
+~~~~~~~~~~~~~~~~~~~~~~~
+
+* Start of the history
+* Add Thai VAT Report
+
+12.0.1.0.1 (2019-09-02)
+~~~~~~~~~~~~~~~~~~~~~~~
+
+* Fix column swap in xlsx report
+* Fix wrong query database to report
+* Add test script
+
+12.0.1.0.2 (2019-10-24)
+~~~~~~~~~~~~~~~~~~~~~~~
+
+* Fix variable name in for loop
