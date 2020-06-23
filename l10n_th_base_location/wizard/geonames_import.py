cf1c947f9150ad00a38179ed5ec3f7423a9b9dc7
Saran440 <saranl@ecosoft.co.th>
2020-06-23 13:49:51 +0700

    [ENH] Add district and sub-district code in city.zip
    3993_6019
    
    [FIX] 1_2528
    
    [ENH] Add district and sub-district code in city.zip
    
    [EDIT]base_location TH_th line 6020

--- l10n_th_base_location/wizard/geonames_import.py
+++ l10n_th_base_location/wizard/geonames_import.py
@@ -18,25 +18,56 @@ class CityZipGeonamesImport(models.TransientModel):
         "please uninstall l10n_th_base_location.",
     )
     location_thailand_language = fields.Selection(
-        [("th", "Thai"), ("en", "English")], string="Language", default="th"
+        [("th", "Thai"), ("en", "English")], string="Language of Thailand", default="th"
     )
 
     @api.depends("country_ids")
     def _compute_is_thailand(self):
         self.ensure_one()
-        if "TH" in self.country_ids.mapped("code"):
-            self.is_thailand = True
-        else:
-            self.is_thailand = False
+        self.is_thailand = "TH" in self.country_ids.mapped("code")
+
+    def _prepare_district_thailand(self, row):
+        sub_district = ""
+        district = ""
+        if len(row) >= 6:
+            district = row[5]
+            if len(row) >= 7:
+                sub_district = row[6]
+        return district, sub_district
+
+    @api.model
+    def prepare_zip(self, row, city_id):
+        vals = super().prepare_zip(row, city_id)
+        district, sub_district = self._prepare_district_thailand(row)
+        vals.update({"district_code": district, "sub_district_code": sub_district})
+        return vals
+
+    @api.model
+    def select_zip(self, row, country, state_id):
+        city_zip = super().select_zip(row, country, state_id)
+        if country.code == "TH":
+            # If District or Sub-District, update code
+            district, sub_district = self._prepare_district_thailand(row)
+            city_zip.write(
+                {"district_code": district, "sub_district_code": sub_district}
+            )
+        return city_zip
 
     @api.model
     def get_and_parse_csv(self, country):
         if country.code == "TH":
             path = os.path.join(os.path.dirname(os.path.abspath(__file__)))
-            if self.location_thailand_language == "th":
-                file_path = os.path.join(path[:-6], "data/TH_th.txt")
+            import_test = self._context.get("import_test", False)
+            if import_test:
+                th_location_file = "demo/TH_th.txt"
+                en_location_file = "demo/TH_en.txt"
             else:
-                file_path = os.path.join(path[:-6], "data/TH_en.txt")
+                th_location_file = "data/TH_th.txt"
+                en_location_file = "data/TH_en.txt"
+            if self.location_thailand_language == "th":
+                file_path = os.path.join(path[:-6], th_location_file)
+            else:
+                file_path = os.path.join(path[:-6], en_location_file)
             data_file = open(file_path, "r", encoding="utf-8")
             data_file.seek(0)
             reader = csv.reader(data_file, delimiter="	")
