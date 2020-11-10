b01b1197ec66b9619e9d34a99d3854798656767c
Poonlap V <poonlap@tanabutr.co.th>
2020-11-10 23:25:33 +0700

    Make code compliant with Pylint-Odoo, Flake8.

--- l10n_th_promptpay/models/payment.py
+++ l10n_th_promptpay/models/payment.py
@@ -1,17 +1,12 @@
-# -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
-# from odoo.addons.payment.models.payment_acquirer import ValidationError
-# from odoo.tools.float_utils import float_compare
+from odoo import fields, models
 from promptpay import qrcode
 
-import logging
-import pprint
 
-
-class l10n_th_promptpay(models.Model):
+class L10nThPromptpay(models.Model):
     _inherit = 'payment.acquirer'
-    qr_code_promptpay =  fields.Boolean('Use PromptPay QR code')
-    promptpay_id = fields.Char(string="PromptPay ID", help="13 digits for company's tax ID or 10 digits for mobile phone number")
+    qr_code_promptpay = fields.Boolean('Use PromptPay QR code')
+    promptpay_id = fields.Char(
+        string="PromptPay ID", help="13 digits for company's tax ID or 10 digits for mobile phone number")
 
     def promptpayPayload(self, data):
-        return qrcode.generate_payload(self.promptpay_id,float(data))
+        return qrcode.generate_payload(self.promptpay_id, float(data))
