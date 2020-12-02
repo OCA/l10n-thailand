4d58298252d781bafc583bda9a4774e7c3cbd51f
Kitti U <kittiu@gmail.com>
2020-12-02 17:06:22 +0700

    [IMP] : black, isort, prettier

--- l10n_th_tax_report/static/src/js/l10n_th_tax_report_backend.js
+++ l10n_th_tax_report/static/src/js/l10n_th_tax_report_backend.js
@@ -1,4 +1,4 @@
-odoo.define("l10n_th_tax_report_backend", function(require) {
+odoo.define("l10n_th_tax_report_backend", function (require) {
     "use strict";
 
     var AbstractAction = require("web.AbstractAction");
@@ -12,7 +12,7 @@ odoo.define("l10n_th_tax_report_backend", function(require) {
             "click .o_l10n_th_tax_report_print": "print",
             "click .o_l10n_th_tax_report_export": "export",
         },
-        init: function(parent, action) {
+        init: function (parent, action) {
             this._super.apply(this, arguments);
             this.actionManager = parent;
             this.given_context = {};
@@ -26,27 +26,27 @@ odoo.define("l10n_th_tax_report_backend", function(require) {
             this.given_context.model = action.context.active_model || false;
             this.given_context.ttype = action.context.ttype || false;
         },
-        willStart: function() {
+        willStart: function () {
             return Promise.all([this._super.apply(this, arguments), this.get_html()]);
         },
-        set_html: function() {
+        set_html: function () {
             var self = this;
             var def = Promise.resolve();
             if (!this.report_widget) {
                 this.report_widget = new ReportWidget(this, this.given_context);
                 def = this.report_widget.appendTo(this.$(".o_content"));
             }
-            def.then(function() {
+            def.then(function () {
                 self.report_widget.$el.html(self.html);
             });
         },
-        start: function() {
+        start: function () {
             this.set_html();
             return this._super();
         },
         // Fetches the html and is previous report.context if any,
         // else create it
-        get_html: function() {
+        get_html: function () {
             var self = this;
             var defs = [];
             return this._rpc({
@@ -54,7 +54,7 @@ odoo.define("l10n_th_tax_report_backend", function(require) {
                 method: "get_html",
                 args: [self.given_context],
                 context: self.odoo_context,
-            }).then(function(result) {
+            }).then(function (result) {
                 self.html = result.html;
                 defs.push(self.update_cp());
                 return Promise.all(defs);
@@ -62,7 +62,7 @@ odoo.define("l10n_th_tax_report_backend", function(require) {
         },
         // Updates the control panel and render the elements that have yet
         // to be rendered
-        update_cp: function() {
+        update_cp: function () {
             if (this.$buttons) {
                 var status = {
                     breadcrumbs: this.actionManager.get_breadcrumbs(),
@@ -71,33 +71,33 @@ odoo.define("l10n_th_tax_report_backend", function(require) {
                 return this.update_control_panel(status);
             }
         },
-        do_show: function() {
+        do_show: function () {
             this._super();
             this.update_cp();
         },
-        print: function() {
+        print: function () {
             var self = this;
             this._rpc({
                 model: this.given_context.model,
                 method: "print_report",
                 args: [this.given_context.active_id, "qweb-pdf"],
                 context: self.odoo_context,
-            }).then(function(result) {
+            }).then(function (result) {
                 self.do_action(result);
             });
         },
-        export: function() {
+        export: function () {
             var self = this;
             this._rpc({
                 model: this.given_context.model,
                 method: "print_report",
                 args: [this.given_context.active_id, "xlsx"],
                 context: self.odoo_context,
-            }).then(function(result) {
+            }).then(function (result) {
                 self.do_action(result);
             });
         },
-        canBeRemoved: function() {
+        canBeRemoved: function () {
             return Promise.resolve();
         },
     });
