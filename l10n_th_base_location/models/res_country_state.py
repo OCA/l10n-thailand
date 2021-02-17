b520de1415dd056c868147afebb5ded4ff13a9aa
Sansiri <sansirih@gmail.com>
2021-02-18 01:07:42 +0700

    Update res_country_state.py

--- l10n_th_base_location/models/res_country_state.py
+++ l10n_th_base_location/models/res_country_state.py
@@ -1,6 +1,8 @@
-# -*- coding: utf-8 -*-
+# Copyright 2021 Sansiri Tanachutiwat
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 from odoo import models
+
 
 class CountryState(models.Model):
     _inherit = "res.country.state"
