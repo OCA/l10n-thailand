711b39084b1cbab23368ce65d59384a8ed40308c
Kitti U <kittiu@gmail.com>
2021-06-27 23:01:53 +0700

    Add WA tier validation and some UI improvement

--- l10n_th_gov_work_acceptance/models/work_acceptance.py
+++ l10n_th_gov_work_acceptance/models/work_acceptance.py
@@ -6,13 +6,37 @@ from odoo import api, fields, models
 
 class WorkAcceptance(models.Model):
     _inherit = "work.acceptance"
+    _tier_validation_manual_config = True  # We need more buttons
 
+    wa_tier_validation = fields.Boolean(
+        string="Paperless WA",
+        readonly=True,
+        states={"draft": [("readonly", False)]},
+        help="If checked, WA created will be approved by committee by tier valiation."
+        "Each committee will be notified (by email or inbox) to approve WA.\n"
+        "If not checked, WA will be approved by paper outside Odoo, "
+        "and the result of WA will be filled in by procurement officer",
+    )
     work_acceptance_committee_ids = fields.One2many(
         comodel_name="work.acceptance.committee",
         inverse_name="wa_id",
         string="Work Acceptance Committees",
         copy=True,
     )
+    completeness = fields.Float(
+        string="Progress",
+        compute="_compute_completeness",
+    )
+
+    @api.depends("work_acceptance_committee_ids.status")
+    def _compute_completeness(self):
+        for rec in self:
+            committees = len(rec.work_acceptance_committee_ids)
+            reviewed = len(rec.work_acceptance_committee_ids.filtered("status"))
+            if committees:
+                rec.completeness = reviewed / committees * 100
+            else:
+                rec.completeness = 0
 
     def _prepare_committee_line(self, line):
         return {
