2837db33c36429a1824084c1fb81249ec539e3d4
Saran440 <saranl@ecosoft.co.th>
2021-08-10 15:48:02 +0700

    [14.0][ADD] l10n_th_expense_withholding_tax

---
+++ l10n_th_expense_withholding_tax/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 Ecosoft Co., Ltd (https://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Thai Localization - Expense Withholding Tax",
+    "version": "14.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "website": "https://github.com/OCA/l10n-thailand",
+    "category": "Localization / Accounting",
+    "depends": ["l10n_th_withholding_tax", "hr_expense"],
+    "data": ["views/hr_expense_view.xml"],
+    "installable": True,
+    "development_status": "Alpha",
+    "maintainers": ["Saran440"],
+}
