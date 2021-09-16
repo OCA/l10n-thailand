c76ee3a0645b3fd437ad4cd3f1d196153f3b1684
Saran440 <saranl@ecosoft.co.th>
2021-09-16 16:55:53 +0700

    [ENH] multi currency and support multi tree view

--- l10n_th_personal_income_tax/readme/USAGE.rst
+++ l10n_th_personal_income_tax/readme/USAGE.rst
@@ -1,14 +1,16 @@
 You can config PIT Rate following,
 
 #. Go to Invoicing > Configuration > Accounting > PIT Rate
-#. Change range or tax rate
+#. Add default field 'Withholding Tax Account' and 'Type of Income' when you selected bills is PIT (if any)
+#. Change range or tax rate (if any)
 
 After you config PIT Rate,
 
 #. Create Vendor Bills
-#. Check Account PIT > Select Type of Income
+#. Check Account PIT. It will default 'Type of Income' from PIT Rate menu
 #. Confirm > Register Payment
-#. It will compute amount withholding tax
-#. If calculated tax with partner and there is a tax, it will auto deduct amount to withholding tax
-#. Check amount and Create Payment
-#. It keep amount income to partner and calculate when you register payment next time
+#. It will compute amount withholding tax. If calculated tax with partner and there is a tax, it will auto deduct amount to withholding tax
+#. Check amount and Click Create Payment
+#. It keep 'amount income' to partner and calculate when you register payment next time
+
+**Note:** You can't register payment between check PIT and not check PIT on 1 time.
--- l10n_th_personal_income_tax/wizard/account_payment_register.py
+++ l10n_th_personal_income_tax/wizard/account_payment_register.py
@@ -12,11 +12,34 @@ from odoo.addons.l10n_th_withholding_tax_cert.models.withholding_tax_cert import
 class AccountPaymentRegister(models.TransientModel):
     _inherit = "account.payment.register"
 
+    @api.onchange("group_payment")
+    def _onchange_group_payment(self):
+        """ Compute payment difference from tree view """
+        model = self._context.get("active_model", False)
+        if model == "account.move" and self.group_payment:
+            self._compute_amount()
+
     def _create_payments(self):
         payments = super()._create_payments()
         for payment in payments:
             move_ids = payment.reconciled_bill_ids.filtered("account_pit")
-            if move_ids:
+            if move_ids and self.group_payment:
+                # Multi-Currency
+                amount_income_currency = sum(move_ids.mapped("amount_untaxed")) or 0.0
+                amount_income = move_ids[0].currency_id._convert(
+                    amount_income_currency,
+                    self.company_id.currency_id,
+                    self.company_id,
+                    self.payment_date,
+                )
+                payment_difference = self.payment_difference
+                if self.company_id.currency_id != self.currency_id:
+                    payment_difference = self.currency_id._convert(
+                        payment_difference,
+                        self.company_id.currency_id,
+                        self.company_id,
+                        self.payment_date,
+                    )
                 payment.write(
                     {
                         "pit_line": [
@@ -29,11 +52,8 @@ class AccountPaymentRegister(models.TransientModel):
                                         0
                                     ].pit_wt_cert_income_type
                                     or False,
-                                    "amount_income": sum(
-                                        move_ids.mapped("amount_untaxed")
-                                    )
-                                    or 0.0,
-                                    "amount_wt": self.payment_difference,
+                                    "amount_income": amount_income,
+                                    "amount_wt": payment_difference,
                                 },
                             )
                         ],
@@ -58,6 +78,21 @@ class AccountPaymentRegister(models.TransientModel):
         )
         return pit.wt_account_id or False
 
+    def _compute_expected_wt(self, move_ids, pit_date=False):
+        PIT = self.env["personal.income.tax"]
+        # Total previous amount personal income tax
+        pit_amount_year = self._get_pit_amount_yearly(self.partner_id)
+        # Multi-Currency: Convert other to main currency
+        base_amount = move_ids[0].currency_id._convert(
+            sum(move_ids.mapped("amount_untaxed")),
+            self.company_id.currency_id,
+            self.company_id,
+            self.payment_date,
+        )
+        total_pit = pit_amount_year + base_amount
+        expected_wt = PIT.calculate_rate_wt(total_pit, base_amount, date=pit_date)
+        return expected_wt
+
     @api.depends(
         "source_amount",
         "source_amount_currency",
@@ -68,29 +103,40 @@ class AccountPaymentRegister(models.TransientModel):
     )
     def _compute_amount(self):
         res = super()._compute_amount()
-        move_id = self.env["account.move"].browse(self._context.get("active_ids", []))
-        if (
-            len(move_id) == 1
-            and move_id.account_pit
-            and not any(line.wt_tax_id for line in move_id.line_ids)
-        ):
-            PIT = self.env["personal.income.tax"]
+        model = self._context.get("active_model", False)
+        batches = self._get_batches()
+        edit_mode = self.can_edit_wizard and (
+            len(batches[0]["lines"]) == 1 or self.group_payment
+        )
+        # Compute amount for case group payment, register payment on form view
+        if model == "account.move" and edit_mode:
+            move_ids = self.env[model].browse(self._context.get("active_ids", []))
+            # Check there isn't account pit on account.move
+            if not any(list(set(move_ids.mapped("account_pit")))):
+                return res
             pit_date = self._context.get("pit_date", False)
             wt_account_id = self._default_pit_wt_account_id(date=pit_date)
-            # Total previous amount personal income tax
-            pit_amount_year = self._get_pit_amount_yearly(self.partner_id)
-            # Amount bills
-            base_amount = move_id.amount_untaxed or 0.0
-            total_pit = pit_amount_year + base_amount
-            expected_wt = PIT.calculate_rate_wt(total_pit, base_amount, date=pit_date)
+            expected_wt = self._compute_expected_wt(move_ids, pit_date)
             if expected_wt:
+                # Convert main to other currency for display amount difference
+                company_currency = self.company_id.currency_id
+                if company_currency != self.currency_id:
+                    expected_wt = company_currency._convert(
+                        expected_wt,
+                        move_ids[0].currency_id,
+                        self.company_id,
+                        self.payment_date,
+                    )
+                list_writeoff_label = [
+                    dict(WHT_CERT_INCOME_TYPE).get(move.pit_wt_cert_income_type, False)
+                    for move in move_ids
+                ]
                 self.amount -= expected_wt
                 self.payment_difference_handling = "reconcile"
                 self.writeoff_account_id = wt_account_id and wt_account_id.id or False
                 self.writeoff_label = (
-                    dict(WHT_CERT_INCOME_TYPE).get(
-                        move_id.pit_wt_cert_income_type, False
-                    )
+                    any(list_writeoff_label)
+                    and ", ".join(list_writeoff_label)
                     or self.writeoff_label
                 )
         return res
@@ -100,9 +146,11 @@ class AccountPaymentRegister(models.TransientModel):
         if self._context.get("active_model") == "account.move":
             active_ids = self._context.get("active_ids", False)
             move_ids = self.env["account.move"].browse(active_ids)
-            account_pits = move_ids.filtered("account_pit")
-            if len(move_ids) > 1 and account_pits:
+            if len(set(move_ids.mapped("account_pit"))) > 1:
                 raise UserError(
-                    _("You can't register a payment on tree view with account pit.")
+                    _(
+                        "You can't register a payment because "
+                        "there is nothing same PIT document."
+                    )
                 )
         return super().default_get(fields_list)
