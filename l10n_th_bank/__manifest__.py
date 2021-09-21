8568e8d8241f71f08bf76f89633cbb86bde3a94c
Saran440 <saranl@ecosoft.co.th>
2021-09-21 16:32:23 +0700

    [14.0][ADD] l10n_th_bank

---
+++ l10n_th_bank/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Ecosoft Co., Ltd (http://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Thai Localization - Bank",
+    "version": "14.0.1.0.0",
+    "category": "Localisation/Asia",
+    "license": "AGPL-3",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-thailand",
+    "depends": ["partner_bank_code"],
+    "data": ["data/res.bank.csv"],
+    "installable": True,
+}
---
+++ l10n_th_bank/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+This module adds all bank of Thailand.
+
+Data Reference : https://www.oic.or.th/sites/default/files/content/4387/bank-code-280157.xlsx
