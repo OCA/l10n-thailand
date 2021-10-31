8b772da0347193542dcbc1036e87b55791939d48
Kitti U <kittiu@gmail.com>
2021-10-31 21:23:37 +0700

    Merge l10n_th_account_advance_clearing to  l10n_th_account_expense_tax

--- l10n_th_account_tax_expense/models/__init__.py
+++ l10n_th_account_tax_expense/models/__init__.py
@@ -2,3 +2,4 @@
 
 from . import account_move
 from . import hr_expense
+from . import hr_expense_sheet
--- l10n_th_account_tax_expense/models/hr_expense_sheet.py
+++ l10n_th_account_tax_expense/models/hr_expense_sheet.py
@@ -1,6 +1,7 @@
 # Copyright 2021 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
-from odoo import fields, models
+from odoo import _, fields, models
+from odoo.exceptions import UserError
 
 
 class HrExpenseSheet(models.Model):
@@ -9,7 +10,7 @@ class HrExpenseSheet(models.Model):
     wht_move_id = fields.Many2one(
         comodel_name="account.move",
         string="Withholding Tax JV",
-        ondelete="restrict",
+        ondelete="set null",
         copy=False,
         readonly=True,
         help="For case clear advance, a JV can be created to record withholding tax",
@@ -25,15 +26,33 @@ class HrExpenseSheet(models.Model):
     def action_create_withholding_tax_entry(self):
         """From expense sheet with WHT lines, this action
         helps create new JV with default withholding entries"""
-        self.ensure_one()
+        active_id = self.env.context.get("active_id")
+        sheet = self.env["hr.expense.sheet"].browse(active_id)
+        sheet.ensure_one()
+        # Validation
+        if sheet.state not in ("done", "post"):
+            raise UserError(_("Only posted or paid expense report can create JV"))
+        if sheet.wht_move_id:
+            raise UserError(_("Already created withholding tax JV"))
+        # Window action
         action = self.env.ref("account.action_move_journal_line")
         result = action.sudo().read()[0]
         view = self.env.ref("account.view_move_form", False)
         result["views"] = [(view and view.id or False, "form")]
+        # Create wht JV
+        move_vals = sheet._prepare_withholding_tax_entry()
+        move = self.env["account.move"].create(move_vals)
+        sheet.wht_move_id = move
+        result["res_id"] = move.id
+        return result
+
+    def _prepare_withholding_tax_entry(self):
+        self.ensure_one()
         # Prepare Dr. Advance, Cr. WHT lines
         line_vals_list = []
+        move_lines = self.account_move_id.mapped("line_ids")
         # Cr. WHT Lines
-        wht_move_lines = self.account_move_id.mapped("line_ids").filtered("wht_tax_id")
+        wht_move_lines = move_lines.filtered("wht_tax_id")
         currency = self.env.company.currency_id
         deduction_list, amount_deduct = wht_move_lines._prepare_deduction_list(
             currency=currency
@@ -48,28 +67,30 @@ class HrExpenseSheet(models.Model):
                     "credit": deduction["amount"],
                     "partner_id": deduction["partner_id"],
                     "account_id": deduction["account_id"],
+                    "wht_tax_id": deduction["wht_tax_id"],
+                    "tax_base_amount": deduction["wht_amount_base"],
                 }
             )
-        emp_advance = self.env.ref("hr_expense_advance_clearing.product_emp_advance")
-        adv_account = emp_advance.property_account_expense_id
-        # Dr. Advance
-        line_vals_list.append(
-            {
-                "name": adv_account.name,
-                "amount_currency": amount_deduct,
-                "currency_id": currency.id,
-                "debit": amount_deduct,  # Sum of all credit
-                "credit": 0.0,
-                "partner_id": self.employee_id.sudo().address_home_id.commercial_partner_id.id,
-                "account_id": adv_account.id,
-            }
-        )
+        accounts = move_lines.mapped("account_id").filtered("reconcile")
+        # Dr. Reconcilable Account (i.e., AP, Advance)
+        partner_id = self.employee_id.sudo().address_home_id.commercial_partner_id.id
+        for account in accounts:
+            line_vals_list.append(
+                {
+                    "name": account.name,
+                    "amount_currency": amount_deduct,
+                    "currency_id": currency.id,
+                    "debit": amount_deduct,  # Sum of all credit
+                    "credit": 0.0,
+                    "partner_id": partner_id,
+                    "account_id": account.id,
+                }
+            )
+            amount_deduct = 0.0
         # Create JV
         move_vals = {
             "move_type": "entry",
+            "ref": self.account_move_id.display_name,
             "line_ids": [(0, 0, line_vals) for line_vals in line_vals_list],
         }
-        move = self.env["account.move"].create(move_vals)
-        self.wht_move_id = move
-        result["res_id"] = move.id
-        return result
+        return move_vals
