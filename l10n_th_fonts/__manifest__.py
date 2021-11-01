a249209f04f7358de9b5cbd0ad6cdc8da36bb4b6
Amin Cheloh <amincheloh@gmail.com>
2021-11-01 14:42:20 +0700

    [IMP] l10n_th_fonts: black, isort, prettier

--- l10n_th_fonts/__manifest__.py
+++ l10n_th_fonts/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Thai Localization - Fonts Thai",
-    "version": "14.0.1.0.2",
+    "version": "15.0.1.0.0",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "summary": "Collection of all fonts thai",
     "license": "AGPL-3",
