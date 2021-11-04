1d554ab54dbe84bab9205f803dfae0d08872a729
Kitti U <kittiu@gmail.com>
2021-11-04 14:08:36 +0700

    FIX, payment see warning banner to create WHT certs, even no wht_tax_id

--- l10n_th_account_tax/models/account_move.py
+++ l10n_th_account_tax/models/account_move.py
@@ -372,10 +372,20 @@ class AccountMove(models.Model):
         ],
         compute="_compute_wht_cert_status",
     )
+    has_wht = fields.Boolean(
+        compute="_compute_has_wht",
+    )
+
+    def _compute_has_wht(self):
+        for rec in self:
+            rec.has_wht = len(rec.line_ids.mapped("wht_tax_id")) > 0
 
     @api.depends("wht_cert_ids.state")
     def _compute_wht_cert_status(self):
         for rec in self:
+            if not rec.has_wht:
+                rec.wht_cert_status = False
+                continue
             if not rec.wht_cert_ids:
                 rec.wht_cert_status = "none"
             elif "draft" in rec.wht_cert_ids.mapped("state"):
