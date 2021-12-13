6caf19d9bb9495bf35713708afc93f6f092c946e
Kitti U <kittiu@ecosoft.co.th>
2021-12-13 16:52:15 +0700

    Init with version 14

---
+++ l10n_th_account_tax_report/__manifest__.py
@@ -0,0 +1,31 @@
+# Copyright 2019 Ecosoft Co., Ltd (https://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Thailand Localization - VAT and Withholding Tax Reports",
+    "version": "14.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-thailand",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "depends": [
+        "date_range",
+        "report_xlsx_helper",
+        "l10n_th_partner",
+        "l10n_th_account_tax",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/paper_format.xml",
+        "data/report_data.xml",
+        "reports/tax_report.xml",
+        "reports/report_wht_qweb.xml",
+        "reports/report_wht_text.xml",
+        "wizard/tax_report_wizard_view.xml",
+        "wizard/withholding_tax_report_wizard_view.xml",
+        "views/account_withholding_tax.xml",
+    ],
+    "installable": True,
+    "development_status": "Beta",
+    "maintainers": ["kittiu"],
+}
