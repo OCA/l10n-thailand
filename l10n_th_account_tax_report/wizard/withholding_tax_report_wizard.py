e0be331c2dc78649df3a64d06c208620e999d285
Saran440 <saranl@ecosoft.co.th>
2022-02-18 20:51:28 +0700

    [FIX] onchange date range

--- l10n_th_account_tax_report/wizard/withholding_tax_report_wizard.py
+++ l10n_th_account_tax_report/wizard/withholding_tax_report_wizard.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Ecosoft Co., Ltd (https://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class WithHoldingTaxReportWizard(models.TransientModel):
@@ -75,3 +75,10 @@ class WithHoldingTaxReportWizard(models.TransientModel):
         model = self.env["withholding.tax.report"]
         report = model.create(self._prepare_wht_report())
         return report.print_report(report_type)
+
+    @api.onchange("date_range_id")
+    def onchange_date_range_id(self):
+        """Handle date range change."""
+        if self.date_range_id:
+            self.date_from = self.date_range_id.date_start
+            self.date_to = self.date_range_id.date_end
