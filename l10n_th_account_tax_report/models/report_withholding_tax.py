1f9b6f796e7b8b698de1f292e952b548002157b8
Saran440 <saranl@ecosoft.co.th>
2022-03-02 16:31:23 +0700

    [UPD] text file -> address and tax branch

--- l10n_th_account_tax_report/models/report_withholding_tax.py
+++ l10n_th_account_tax_report/models/report_withholding_tax.py
@@ -35,7 +35,85 @@ class WithHoldingTaxReport(models.TransientModel):
         help="Use compute fields, so there is nothing store in database",
     )
 
+    def _get_name_filter(self, title=False, firstname=False, lastname=False):
+        return "{}|{}|{}".format(title or "", firstname or "", lastname or "")
+
+    def find_information(self, partner_id, line):
+        # PND53
+        if line.wht_income_tax_form == "pnd53":
+            name = "{}".format(
+                partner_id.company_type == "person"
+                and (partner_id.firstname or partner_id.lastname)
+                or partner_id.name_company
+            )
+        # PND3, PND1
+        else:
+            name = (
+                partner_id.company_type == "person"
+                and self._get_name_filter(
+                    title=partner_id.title.name,
+                    firstname=partner_id.firstname,
+                    lastname=partner_id.lastname,
+                )
+                or self._get_name_filter(firstname=partner_id.name_company)
+            )
+        return name
+
+    def find_address(self, partner_id, line):
+        if line.wht_income_tax_form == "pnd1":
+            return ""
+        return "|".join(
+            [
+                partner_id.street or "",
+                partner_id.street2 or "",
+                partner_id.city or "",
+                partner_id.state_id.name or "",
+                partner_id.zip or "",
+            ]
+        )
+
     def _create_text(self, docs):
+        """Text File PND
+        - PND1 :
+            1. income code
+            2. index
+            3. vat
+            4. title
+            5. firstname
+            6. lastname
+            7. date
+            8. type of income
+            9. base amount
+            10. wht amount
+            11. tax payer
+
+        - PND3 :
+            1. index
+            2. vat
+            3. title
+            4. firstname
+            5. lastname
+            6-10. Street, Street2, City, State ID, zip
+            11. date
+            12. type of income (description)
+            13. wht percent
+            14. base amount
+            15. wht amount
+            16. tax payer
+
+        - PND53 :
+            1. index
+            2. vat
+            3. name
+            4-8. Street, Street2, City, State ID, zip
+            9. date
+            10. type of income (description)
+            11. wht percent
+            12. base amount
+            13. wht amount
+            14. tax payer
+            15. tax branch
+        """
         self.ensure_one()
         for obj in docs:
             text = ""
@@ -47,43 +125,37 @@ class WithHoldingTaxReport(models.TransientModel):
                 )
                 partner_id = line.cert_id.partner_id
                 vat = partner_id.vat or ""
-                type_income_desc = "{}|{}|".format(
-                    line.wht_cert_income_desc, int(line.wht_percent)
-                )
-                if line.wht_income_tax_form == "pnd53":
-                    name = "{}".format(
-                        partner_id.company_type == "person"
-                        and partner_id.firstname
-                        or partner_id.name_company
+                type_income_desc = (
+                    line.wht_income_tax_form != "pnd1"
+                    and "{}|{}|".format(
+                        line.wht_cert_income_desc, int(line.wht_percent)
                     )
-                elif line.wht_income_tax_form == "pnd3":
-                    fullname = " ".join([partner_id.firstname, partner_id.lastname])
-                    if partner_id.company_type == "person":
-                        name = "{}|{}".format(partner_id.title.name or "", fullname)
-                    else:
-                        name = "{}|{}".format("", partner_id.name_company)
-                else:
-                    if partner_id.company_type == "person":
-                        name = "{}|{}|{}".format(
-                            partner_id.title.name or "",
-                            partner_id.firstname or "",
-                            partner_id.lastname or "",
-                        )
-                    else:
-                        name = "{}|{}|{}".format("", partner_id.name_company, "")
-                    type_income_desc = ""
+                    or ""
+                )
+                name = obj.find_information(partner_id, line)
+                address = obj.find_address(partner_id, line)
+                tax_branch = (
+                    line.wht_income_tax_form == "pnd53"
+                    and "|{}".format(partner_id.branch or "")
+                    or ""
+                )
                 text += (
-                    "{income_code}{index}|{vat}|{name}|{date}|{type_income_desc}"
-                    "{base_amount}|{wht_amount}|{tax_payer}\n".format(
+                    "{income_code}{index}|{vat}|{name}|{address}{date}|"
+                    "{type_income_desc}{base_amount}|{wht_amount}|{tax_payer}"
+                    "{tax_branch}\n".format(
                         income_code=income_code,  # for pnd1 only
                         index=idx + 1,
                         vat=vat,
                         name=name,
+                        address=address
+                        and "{}|".format(address)
+                        or "",  # pnd3, 53 (optional)
                         date=self.format_date_dmy(line.cert_id.date),
                         type_income_desc=type_income_desc,
                         base_amount="{:,.2f}".format(line.base) or 0.00,
                         wht_amount="{:,.2f}".format(line.amount) or 0.00,
                         tax_payer=self._convert_tax_payer(line.cert_id.tax_payer),
+                        tax_branch=tax_branch,
                     )
                 )
         return text
