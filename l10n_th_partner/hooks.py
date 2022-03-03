19d10fe583697fee32106ddfcdad477a3a7c9e09
Kitti U <kittiu@ecosoft.co.th>
2022-03-03 14:25:29 +0700

    [15.0][MIG] l10n_th_partner

--- l10n_th_partner/hooks.py
+++ l10n_th_partner/hooks.py
@@ -4,6 +4,5 @@ from odoo import SUPERUSER_ID, api
 
 
 def post_init_hook(cr, _):
-    with api.Environment.manage():
-        env = api.Environment(cr, SUPERUSER_ID, {})
-        env["res.partner"]._install_l10n_th_partner()
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    env["res.partner"]._install_l10n_th_partner()
