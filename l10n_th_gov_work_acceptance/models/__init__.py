4f08c85aa89063babe1262188ffd05c90b3487af
Saran440 <saranl@ecosoft.co.th>
2022-05-11 11:37:57 +0700

    [FIX] restart wa committee

--- l10n_th_gov_work_acceptance/models/__init__.py
+++ l10n_th_gov_work_acceptance/models/__init__.py
@@ -1,4 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
 from . import work_acceptance_committee
 from . import work_acceptance
 from . import purchase
+from . import tier_validation
