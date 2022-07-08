bab8b323a0a0236b50376bfb6d20af3831a707cc
sangcrazy4 <sang_sakarin@outlook.com>
2022-07-08 15:29:38 +0700

    bump: l10n_th_withholding_tax_report 14.0 to 15.0

---
+++ l10n_th_withholding_tax_report/__manifest__.py
@@ -0,0 +1,43 @@
+# Copyright 2019 Ecosoft Co., Ltd (https://ecosoft.co.th/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Thailand Localization - Withholding Tax Report",
+    "version": "15.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-thailand",
+    "license": "AGPL-3",
+    "category": "Accounting",
+    "depends": [
+        "account",
+        "report_xlsx_helper",
+        "date_range",
+        "l10n_th_partner",
+        "l10n_th_withholding_tax_cert",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "data/paper_format.xml",
+        "data/report_data.xml",
+        "templates/layouts.xml",
+        "templates/report_template.xml",
+        "report/report_withholding_tax_qweb.xml",
+        "wizard/withholding_tax_report_wizard_view.xml",
+        "views/menu.xml",
+    ],
+    "installable": True,
+    "application": True,
+    "assets": {
+        'web.assets_backend': [
+        ],
+        'web.report_assets_common': [
+            'l10n_th_withholding_tax_report/static/src/js/withholding_tax_report_backend.js',
+            'l10n_th_withholding_tax_report/static/src/js/withholding_tax_report_widgets.js',
+            'l10n_th_withholding_tax_report/static/src/css/report.css'
+        ],
+        'web.assets_frontend': [
+        ],
+        'web.assets_qweb': [
+        ]
+    }
+}
