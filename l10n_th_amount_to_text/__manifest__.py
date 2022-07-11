38b2bcc5c03d0921d7960622572db47b8b21db9b
sakarin <sakarin.s@neosmart.co.th>
2022-07-11 10:01:09 +0700

    feat: use oca addons with submodule

---
+++ l10n_th_amount_to_text/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Thai Localization - Convert Amount Text to Thai",
+    "version": "14.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-thailand",
+    "license": "AGPL-3",
+    "category": "Localization",
+    "summary": "Convert Amount Text to Thai",
+    "depends": ["base"],
+    "external_dependencies": {"python": ["num2words"]},
+    "data": [],
+    "installable": True,
+    "development_status": "Alpha",
+    "maintainers": ["Saran440"],
+}
