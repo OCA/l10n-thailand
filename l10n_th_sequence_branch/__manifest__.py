75f8ef7901a9dcda0fd67c1007d4df86121a9a41
Saran440 <saranl@ecosoft.co.th>
2022-09-15 17:36:30 +0700

    [15.0][MIG] l10n_th_sequence_branch

--- l10n_th_sequence_branch/__manifest__.py
+++ l10n_th_sequence_branch/__manifest__.py
@@ -1,6 +1,8 @@
+# Licensed AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 {
     "name": "Thai Localization - Sequence with Company Branch",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Localization",
     "website": "https://github.com/OCA/l10n-thailand",
     "author": "Sansiri Tanachutiwat, Odoo Community Association (OCA)",
@@ -8,7 +10,6 @@
         "base",
         "l10n_th_partner",
         "l10n_th_sequence_refactored",
-        "l10n_th_sequence_preview",
     ],
     "data": ["views/ir_sequence_view.xml"],
     "license": "AGPL-3",
