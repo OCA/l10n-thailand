26aa31dc6f53d95a7a4ae1be71707bca65a5c460
Saran440 <saranl@ecosoft.co.th>
2022-09-15 11:16:56 +0700

    [15.0][MIG] l10n_th_sequence_range_end

--- l10n_th_sequence_range_end/__manifest__.py
+++ l10n_th_sequence_range_end/__manifest__.py
@@ -1,6 +1,8 @@
+# Licensed AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
 {
     "name": "Sequence - Range End",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Localization",
     "website": "https://github.com/OCA/l10n-thailand",
     "author": "Sansiri Tanachutiwat, Odoo Community Association (OCA)",
