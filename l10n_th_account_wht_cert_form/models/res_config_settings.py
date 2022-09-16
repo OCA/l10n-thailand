8e1d2dfaf1410a65a8049ad82c589fabc619f384
Saran440 <saranl@ecosoft.co.th>
2022-09-16 17:22:43 +0700

    [RFC] l10n_th_account_wht_cert_form

---
+++ l10n_th_account_wht_cert_form/models/res_config_settings.py
@@ -0,0 +1,11 @@
+# Copyright 2022 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    wht_form_preprint = fields.Boolean(
+        related="company_id.wht_form_preprint", readonly=False
+    )
