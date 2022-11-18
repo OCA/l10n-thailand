ee506d7fde7f4f499d91334976a52ced9999eae2
Amin Cheloh <amincheloh@gmail.com>
2022-11-18 14:17:53 +0700

    [MIG] l10n_th_fonts: Migration to 16.0

--- l10n_th_fonts/__manifest__.py
+++ l10n_th_fonts/__manifest__.py
@@ -2,10 +2,10 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
 {
-    "name": "Thai Localization - Fonts Thai",
-    "version": "15.0.1.0.0",
+    "name": "Thai Localization - Thai Fonts",
+    "version": "16.0.1.0.0",
     "author": "Ecosoft, Odoo Community Association (OCA)",
-    "summary": "Collection of all fonts thai",
+    "summary": "Collection of all Thai fonts",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/l10n-thailand",
     "category": "Report",
