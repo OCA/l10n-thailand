21b1cf610de7b0cad166220efd25d68ae8d4b7f3
Saran440 <saranl@ecosoft.co.th>
2022-11-28 16:13:07 +0700

    [FIX] bug name company duplicate

--- l10n_th_partner/tests/test_l10n_th_partner.py
+++ l10n_th_partner/tests/test_l10n_th_partner.py
@@ -10,6 +10,7 @@ class TestL10nThPartner(TransactionCase):
         self.main_company = self.env.ref("base.main_company")
         self.create_title()
         self.create_original("Firstname", "Lastname")
+        self.company_type = self.env.ref("l10n_th_partner.company_type_3")
 
     def create_title(self):
         self.title = self.env["res.partner.title"].create(
@@ -31,14 +32,31 @@ class TestL10nThPartner(TransactionCase):
         self.assertEqual(self.user.name, "Miss Firstname Lastname")
 
     def test_res_partner(self):
-        """Test that you change company_type"""
+        """Test all of partner individual"""
         partner = self.user.partner_id
         partner.email = "test"
         partner.title = self.title
+        # change individual -> company
         self.assertEqual(partner.title, self.title)
-        partner.company_type = "company"
-        partner._onchange_company_type()
+        self.assertFalse(partner.name_company)
+        with Form(partner) as p:
+            p.company_type = "company"
+            p.name_company = ("Test Company",)
         self.assertNotEqual(partner.title, self.title)
+        self.assertTrue(partner.name_company)
+
+    def test_res_partner_company(self):
+        """Test all of partner company"""
+        with Form(
+            self.env["res.partner"], view="partner_firstname.view_partner_form"
+        ) as f:
+            f.company_type = "company"
+            f.name_company = ("Test Company",)
+            f.partner_company_type_id = self.company_type
+        partner = f.save()
+        # name include title, name_company not include title
+        self.assertTrue(partner.name_company)
+        self.assertNotEqual(partner.name, partner.name_company)
 
     def test_res_users_config_no_space(self):
         """Test that you change title and config title no space"""
