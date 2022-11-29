4bbad6f1fbcd1e060d504ba8b0c0a721cfeeeae9
Kitti U <kittiu@ecosoft.co.th>
2022-11-29 12:09:56 +0700

    [14.0][ADD] l10n_th_gov_work_acceptance

---
+++ l10n_th_gov_work_acceptance/models/purchase.py
@@ -0,0 +1,37 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class PurchaseOrder(models.Model):
+    _inherit = "purchase.order"
+
+    wa_tier_validation = fields.Boolean(
+        string="Paperless WA",
+        default=True,
+        help="If checked, WA created will be approved by committee by tier valiation."
+        "Each committee will be notified (by email or inbox) to approve WA.\n"
+        "If not checked, WA will be approved by paper outside Odoo, "
+        "and the result of WA will be filled in by procurement officer",
+    )
+
+    def _prepare_committee_line(self, line):
+        return {
+            "employee_id": line.employee_id.id,
+            "approve_role": line.approve_role,
+            "note": line.note,
+        }
+
+    def _get_committee_line(self, purchase_requests):
+        committees = purchase_requests.mapped("work_acceptance_committee_ids")
+        lines = [(0, 0, self._prepare_committee_line(line)) for line in committees]
+        return lines
+
+    def action_view_wa(self):
+        result = super().action_view_wa()
+        purchase_requests = self.order_line.mapped("purchase_request_lines.request_id")
+        lines = self._get_committee_line(purchase_requests)
+        result["context"]["default_work_acceptance_committee_ids"] = lines
+        result["context"]["default_wa_tier_validation"] = self.wa_tier_validation
+        return result
---
+++ l10n_th_gov_work_acceptance/readme/DESCRIPTION.rst
@@ -0,0 +1,6 @@
+This module contain process related to purchase's work acceptance, including,
+
+* WA acceptance is required if purchase request is selected.
+* Add Committee tab to WA document, and if any, pull committee from purchase request created from purchase request
+* Work Acceptance Evaluation with criterias
+* Work Acceptance Late Fines, on the last acceptance.
