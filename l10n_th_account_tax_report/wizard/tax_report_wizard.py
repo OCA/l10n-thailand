071b309fe1755217952221332fea54b1bfa2f1b4
Saran440 <saranl@ecosoft.co.th>
2022-12-06 12:08:00 +0700

    [15.0][MIG/REF] l10n_th_account_tax_report

--- l10n_th_account_tax_report/wizard/tax_report_wizard.py
+++ l10n_th_account_tax_report/wizard/tax_report_wizard.py
@@ -1,7 +1,8 @@
 # Copyright 2019 Ecosoft Co., Ltd (https://ecosoft.co.th)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import UserError
 
 
 class TaxReportWizard(models.TransientModel):
@@ -26,16 +27,26 @@ class TaxReportWizard(models.TransientModel):
             ("include_base_amount", "=", False),
         ],
     )
-    date_range_id = fields.Many2one(
-        comodel_name="date.range", string="Period", required=True
+    date_range_id = fields.Many2one(comodel_name="date.range", string="Period")
+    date_from = fields.Date(required=True)
+    date_to = fields.Date(required=True)
+    show_cancel = fields.Boolean(
+        string="Show Cancelled",
+        default=True,
     )
+
+    @api.constrains("date_from", "date_to")
+    def check_date_from_to(self):
+        for rec in self:
+            if rec.date_from and rec.date_to and rec.date_from > rec.date_to:
+                raise UserError(_("Date From must not be after Date To"))
 
     def button_export_html(self):
         self.ensure_one()
         action = self.env.ref(
             "l10n_th_account_tax_report.action_report_tax_report_html"
         )
-        vals = action.read()[0]
+        vals = action.sudo().read()[0]
         context1 = {"active_model": "report.tax.report"}
         model = self.env["report.tax.report"]
         report = model.create(self._prepare_tax_report())
@@ -59,12 +70,20 @@ class TaxReportWizard(models.TransientModel):
         return {
             "company_id": self.company_id.id,
             "tax_id": self.tax_id.id,
-            "date_range_id": self.date_range_id.id,
-            "date_from": self.date_range_id.date_start,
-            "date_to": self.date_range_id.date_end,
+            "date_range_id": self.date_range_id and self.date_range_id.id or False,
+            "date_from": self.date_from,
+            "date_to": self.date_to,
+            "show_cancel": self.show_cancel,
         }
 
     def _export(self, report_type):
         model = self.env["report.tax.report"]
         report = model.create(self._prepare_tax_report())
         return report.print_report(report_type)
+
+    @api.onchange("date_range_id")
+    def onchange_date_range_id(self):
+        """Handle date range change."""
+        if self.date_range_id:
+            self.date_from = self.date_range_id.date_start
+            self.date_to = self.date_range_id.date_end
