966aa7e697256a57352d04b88d96d5fa64431d08
Saran440 <saranl@ecosoft.co.th>
2022-12-09 13:35:34 +0700

    [15.0][ADD] l10n_th_bank_payment_export

---
+++ l10n_th_bank_payment_export/models/__init__.py
@@ -0,0 +1,8 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import bank_payment_config
+from . import account_payment
+from . import account_journal
+from . import bank_payment_export_line
+from . import bank_payment_export
