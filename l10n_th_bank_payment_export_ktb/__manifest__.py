8bf890417ec0ec9863df57a6efd805dacafad216
Saran440 <saranl@ecosoft.co.th>
2022-12-13 16:35:16 +0700

    [15.0][ADD] l10n_th_bank_payment_export_ktb

---
+++ l10n_th_bank_payment_export_ktb/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Ecosoft Co., Ltd. (http://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Thai Localization - Bank Payment Export KTB",
+    "version": "15.0.1.0.0",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/l10n-thailand",
+    "license": "AGPL-3",
+    "category": "Localization / Accounting",
+    "summary": "Bank Payment Export File KTB",
+    "depends": ["l10n_th_bank_payment_export"],
+    "data": [
+        "data/report_action.xml",
+        "views/bank_payment_export_view.xml",
+    ],
+    "installable": True,
+    "development_status": "Alpha",
+    "maintainers": ["Saran440"],
+}
---
+++ l10n_th_bank_payment_export_ktb/readme/CONFIGURE.rst
@@ -0,0 +1,7 @@
+To configure this module, you need to setup
+your `KTB Company ID` and `KTB Sender Name` first:
+
+#. Go to *Invoicing > Configuration > Payments > Bank Payment Configuration*.
+#. Select `Field` and add your value
+#. Select `Journal` field, if you want to default on Bank Payment Export following journal
+#. Check `default` field, if you want to default on Bank Payment Export menu
