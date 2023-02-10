b88bd123d8efa9125049cd3718e8c342fd15e136
Saran440 <saranl@ecosoft.co.th>
2023-02-10 16:44:18 +0700

    [FIX] l10n_th_partner: filter instead of search compute field

--- l10n_th_partner/hooks.py
+++ l10n_th_partner/hooks.py
@@ -12,12 +12,7 @@ _logger = logging.getLogger(__name__)
 def post_init_hook(cr, _):
     """Update partner company field name_company is equal name"""
     env = Environment(cr, SUPERUSER_ID, {})
-    partner_company = env["res.partner"].search(
-        [
-            ("company_type", "=", "company"),
-            ("name_company", "=", False),
-        ]
-    )
-    for partner in partner_company:
+    partner_company = env["res.partner"].search([("name_company", "=", False)])
+    for partner in partner_company.filtered(lambda l: l.company_type == "company"):
         partner.name_company = partner.name
     _logger.info("partners updated installing module.")
