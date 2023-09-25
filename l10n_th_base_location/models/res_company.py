6e69f2150ab1100a53c98891c0d60a3758b35bc7
Saran440 <saranl@ecosoft.co.th>
2023-09-25 16:08:15 +0700

    [MIG] l10n_th_base_location: Migration to 16.0

--- l10n_th_base_location/models/res_company.py
+++ l10n_th_base_location/models/res_company.py
@@ -7,11 +7,6 @@ from odoo import api, models
 class ResCompany(models.Model):
     _inherit = "res.company"
 
-    def _inverse_street2(self):
-        return super(
-            ResCompany, self.with_context(skip_check_zip=True)
-        )._inverse_street2()
-
     @api.onchange("zip_id")
     def _onchange_zip_id(self):
         res = super()._onchange_zip_id()
