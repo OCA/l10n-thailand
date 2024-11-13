68a5c16224139e5f5a6f05d0961a7c94008858ce
saran <Saran440>
2024-11-13 15:54:27 +0700

    [IMP] currency_rate_update_TH_BOT: pre-commit auto fixes

--- currency_rate_update_TH_BOT/models/res_currency_rate_provider_BOT.py
+++ currency_rate_update_TH_BOT/models/res_currency_rate_provider_BOT.py
@@ -144,7 +144,7 @@ class ResCurrencyRateProviderBOT(models.Model):
             content = dict()
             for bot_currency in bot_currencies:
                 currency = bot_currency.bot_currency_name
-                url = "{}&currency={}".format(url, currency)
+                url = f"{url}&currency={currency}"
                 response = requests.get(url, headers=headers, timeout=15)
                 data_dict = response.json()
                 result = data_dict.get("result", False)
