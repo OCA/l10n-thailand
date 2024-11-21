f311e653e9cf8a8d05010d54606a55241944150c
Saran440 <saranl@ecosoft.co.th>
2024-11-21 14:20:52 +0700

    [MIG] l10n_th_fonts: Migration to 18.0

--- l10n_th_fonts/__manifest__.py
+++ l10n_th_fonts/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Thai Localization - Thai Fonts",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Ecosoft, Odoo Community Association (OCA)",
     "summary": "Collection of all Thai fonts",
     "license": "AGPL-3",
