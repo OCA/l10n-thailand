a6f9ab9e244fce9528f298ec3ad5e63b9b4d0a48
Baptiste Pellarin <baptiste@newlogic.com>
2025-01-27 12:08:45 +0700

    [IMP] l10n_th_base_sequence: pre-commit auto fixes

--- l10n_th_base_sequence/__manifest__.py
+++ l10n_th_base_sequence/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Thai Localization - Base Sequence",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "category": "Tools",
     "website": "https://github.com/OCA/l10n-thailand",
     "author": "Sansiri Tanachutiwat, Ecosoft, Odoo Community Association (OCA)",
