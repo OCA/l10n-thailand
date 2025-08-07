bbf009bd9a2dd754f1c01b07ed3a57c0a5ec5f94
Saran440 <saranl@ecosoft.co.th>
2025-08-07 13:59:14 +0700

    [FIX] l10n_th_account_tax: create cashbasis without sequence

--- l10n_th_account_tax/models/account_partial_reconcile.py
+++ l10n_th_account_tax/models/account_partial_reconcile.py
@@ -7,6 +7,18 @@ from odoo import models
 class AccountPartialReconcile(models.Model):
     _inherit = "account.partial.reconcile"
 
+    def _get_move_type_cash_basis(self):
+        return ["in_invoice", "entry"]
+
+    def _update_state_cash_basis(self, moves):
+        """Back state tax cash basis in bills and entry to draft
+        not include net refund and payment. waiting clear tax later."""
+        move_type = self._get_move_type_cash_basis()
+        for move in moves:
+            if move.tax_cash_basis_origin_move_id.move_type in move_type:
+                move.mapped("line_ids").remove_move_reconcile()
+                move.write({"state": "draft", "is_move_sent": False})
+
     def _create_tax_cash_basis_moves(self):
         """This method is called from the move lines that
         create cash basis entry. We want to use the same payment_id when
@@ -53,4 +65,17 @@ class AccountPartialReconcile(models.Model):
                 "DELETE FROM account_move_line WHERE id in %s",
                 (tuple(del_move_lines.ids),),
             )
+
+        # Case: Vendor Bills only.
+        # Reset tax cash basis to draft. until clear tax or reset payment
+        # - Bill --> Payment
+        #     create cash basis (1) is draft
+        # - Payment (Posted) --> Payment (Draft)
+        #     create cash basis (2) and reconcile with (1) state change to posted
+        # - Bill manual reconcile payment
+        #     create cash basis (3) is draft
+        net_invoice_refund = self.env.context.get("net_invoice_refund")
+        net_invoice_payment = self.env.context.get("net_invoice_payment")
+        if not net_invoice_refund or net_invoice_payment:
+            self._update_state_cash_basis(moves)
         return moves
