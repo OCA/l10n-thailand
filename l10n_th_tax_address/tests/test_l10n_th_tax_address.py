1cb088ad12168245e76300764d085359f8d7d642
theerayutEncoder <theerayutattajak@gmail.com>
2025-11-20 11:29:37 +0700

    [IMP] l10n_th_tax_address: Migration to 18.0

--- l10n_th_tax_address/tests/test_l10n_th_tax_address.py
+++ l10n_th_tax_address/tests/test_l10n_th_tax_address.py
@@ -1,6 +1,8 @@
 # Copyright 2024 Ecosoft Co., Ltd (http://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
+from unittest.mock import patch
+
 from odoo.exceptions import ValidationError
 from odoo.tests.common import TransactionCase
 
@@ -10,6 +12,8 @@ class TestTaxAddress(TransactionCase):
     def setUpClass(cls):
         super().setUpClass()
         cls.partner = cls.env.ref("base.res_partner_12")
+        cls.partner.vat = ""
+        cls.partner.branch = ""
 
     def test_01_tax_address_check_constraint(self):
         self.assertFalse(self.partner.vat)
@@ -29,8 +33,20 @@ class TestTaxAddress(TransactionCase):
         with self.assertRaises(ValidationError):
             self.partner.action_get_address()
 
-    def test_02_tax_address(self):
+    @patch(
+        "odoo.addons.l10n_th_tax_address.models.res_partner.ResPartner.get_result_tax_address"
+    )
+    def test_02_tax_address(self, mock_get_result):
         """Test with Ecosoft tax id"""
+        # Mock the API response
+        mock_get_result.return_value = {
+            "street": "459/132 หมู่บ้าน โครงการ นิว ไฮบ์ สุขสวัสดิ์ ถนนสุขสวัสดิ์",
+            "street2": "แขวงราษฎร์บูรณะ",
+            "city": "เขตราษฎร์บูรณะ",
+            "state_id": False,
+            "zip": "10140",
+        }
+
         self.partner.vat = "0105554048641"
         self.partner.branch = "00000"
 
@@ -50,3 +66,46 @@ class TestTaxAddress(TransactionCase):
         # if create master data it will return 'กรุงเทพมหานคร'
         self.assertEqual(self.partner.state_id.name, False)
         self.assertEqual(self.partner.zip, "10140")
+
+    @patch(
+        "odoo.addons.l10n_th_tax_address.models.res_partner.ResPartner.get_result_tax_address"
+    )
+    def test_03_tax_address_api_error(self, mock_get_result):
+        """Test API error handling"""
+        mock_get_result.side_effect = ValidationError("API Error")
+
+        self.partner.vat = "0105554048641"
+        self.partner.branch = "00000"
+
+        with self.assertRaises(ValidationError):
+            self.partner.action_get_address()
+
+    @patch(
+        "odoo.addons.l10n_th_tax_address.models.res_partner.ResPartner.get_result_tax_address"
+    )
+    def test_04_tax_address_with_state(self, mock_get_result):
+        """Test with state_id present"""
+        state = self.env["res.country.state"].create(
+            {
+                "name": "กรุงเทพมหานคร",
+                "code": "BKK",
+                "country_id": self.env.ref("base.th").id,
+            }
+        )
+
+        mock_get_result.return_value = {
+            "street": "123 Test Street",
+            "street2": "Test District",
+            "city": "Test City",
+            "state_id": state.id,
+            "zip": "10000",
+        }
+
+        self.partner.vat = "0105554048641"
+        self.partner.branch = "00000"
+
+        self.partner.action_get_address()
+
+        self.assertEqual(self.partner.street, "123 Test Street")
+        self.assertEqual(self.partner.state_id.id, state.id)
+        self.assertEqual(self.partner.zip, "10000")
