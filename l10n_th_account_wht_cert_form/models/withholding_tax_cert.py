4b3825eebfd07b129f99bff8b09d94913d2b22b5
monthop-gmail <monthop-gmail@users.noreply.github.com>
2026-03-16 17:39:13 +0700

    [MIG] l10n_th_account_wht_cert_form: Migration to 19.0

--- l10n_th_account_wht_cert_form/models/withholding_tax_cert.py
+++ l10n_th_account_wht_cert_form/models/withholding_tax_cert.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Ecosoft Co., Ltd (https://ecosoft.co.th/)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
 
-from odoo import _, models
+from odoo import models
 
 
 class WithholdingTaxCert(models.Model):
@@ -9,7 +9,10 @@ class WithholdingTaxCert(models.Model):
 
     def _get_report_base_filename(self):
         self.ensure_one()
-        return _("WHT Certificates - {name}").format(name=self.display_name or False)
+        return self.env._(
+            "WHT Certificates - %(name)s",
+            name=self.display_name or False,
+        )
 
     def _compute_desc_type_other(self, lines, ttype, income_type):
         base_type_other = lines.filtered(
@@ -20,10 +23,16 @@ class WithholdingTaxCert(models.Model):
         return desc
 
     def _group_wht_line(self, lines):
-        groups = self.env["withholding.tax.cert.line"].read_group(
+        groups = self.env["withholding.tax.cert.line"]._read_group(
             domain=[("id", "in", lines.ids)],
-            fields=["wht_cert_income_type", "base", "amount"],
             groupby=["wht_cert_income_type"],
-            lazy=False,
+            aggregates=["base:sum", "amount:sum"],
         )
-        return groups
+        return [
+            {
+                "wht_cert_income_type": wht_cert_income_type,
+                "base": base_sum,
+                "amount": amount_sum,
+            }
+            for wht_cert_income_type, base_sum, amount_sum in groups
+        ]
